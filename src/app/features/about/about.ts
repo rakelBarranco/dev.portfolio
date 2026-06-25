58daf99f880ab6b39641375ea3401d8a47485e1f
Raquel Barranco <raquelbarranco02@gmail.com>
2026-06-25 14:33:33 +0200

    feat: add about section with experience timeline

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {
+export class AboutComponent {
 
+  experience = [
+    {
+      role: 'Desarrolladora Frontend en prácticas',
+      company: 'Disid',
+      period: 'Marzo 2026 – Junio 2026',
+      items: [
+        'Desarrollo de aplicaciones con Angular y TypeScript',
+        'Consumo de APIs REST',
+        'Trabajo con Git y GitHub',
+        'Participación en proyecto real con equipos frontend y backend',
+        'Metodologías ágiles (Scrum/Kanban)'
+      ]
+    },
+    {
+      role: 'Diseñadora en prácticas',
+      company: 'Tuateam',
+      period: 'Marzo 2023 – Junio 2023',
+      items: [
+        'Diseño 3D',
+        'Edición de vídeo'
+      ]
+    }
+  ];
 }
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import HeroComponent from '../hero/hero';
 import {Projects} from '../projects/projects';
+import {AboutComponent} from '../about/about';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeroComponent,
-    Projects
+    Projects,
+    AboutComponent
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
